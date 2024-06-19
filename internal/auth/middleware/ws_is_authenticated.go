add54c087ce01df2852f06c54ecca8042846fad4
tlugat <teo.lugat@ausha.co>
2024-06-19 20:31:57 +0200

    ✨ Init event room messages

---
+++ internal/auth/middleware/ws_is_authenticated.go
@@ -0,0 +1,71 @@
+package middleware
+
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt/v5"
+	"gorm.io/gorm"
+	"net/http"
+	"os"
+	"sharePie-api/internal/models"
+	"strings"
+	"time"
+)
+
+func WSIsAuthenticated(db *gorm.DB) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		authorization := c.Query("authorization")
+		if authorization == "" {
+			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "Authorization header is required"})
+			return
+		}
+
+		splitToken := strings.Split(authorization, "Bearer ")
+		if len(splitToken) != 2 {
+			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "Invalid token format"})
+			return
+		}
+		tokenString := splitToken[1]
+		fmt.Println("tokenString", tokenString)
+		// Decode/validate it
+		token, _ := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+			// Don't forget to validate the alg is what you expect:
+			if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+				return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
+			}
+
+			// hmacSampleSecret is a []byte containing your secret, e.g. []byte("my_secret_key")
+			return []byte(os.Getenv("JWT_SECRET")), nil
+		})
+
+		if token == nil {
+			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "Invalid token"})
+			return
+		}
+
+		if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
+			// Check the expiry date
+			if float64(time.Now().Unix()) > claims["exp"].(float64) {
+				c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "Token expired"})
+				return
+			}
+
+			// Find the user with token Subject
+			var user models.User
+			db.First(&user, claims["sub"])
+
+			if user.ID == 0 {
+				c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "User not found"})
+				return
+			}
+
+			// Attach the request
+			c.Set("user", user)
+
+			//Continue
+			c.Next()
+		} else {
+			c.AbortWithStatus(http.StatusUnauthorized)
+		}
+	}
+}
--- pkg/config/thirdparty/websocket/server.go
+++ pkg/config/thirdparty/websocket/server.go
@@ -1,10 +1,13 @@
 package websocket
 
 import (
+	"encoding/json"
+	"fmt"
 	"github.com/gin-gonic/gin"
 	"github.com/gorilla/websocket"
 	"net/http"
 	"sharePie-api/internal/auth"
+	"strconv"
 )
 
 var upgrader = websocket.Upgrader{
@@ -34,6 +37,97 @@ func ServeWs(hub *Hub, c *gin.Context) {
 	}
 	client.hub.register <- client
 
+	initRoomMessages(client)
+
 	go client.writePump()
 	go client.readPump()
+}
+
+// Fetch event, users, and expenses of the event and send the 'event', 'users', and 'expenses' events to the room
+func initRoomMessages(client *Client) {
+	eventId, err := strconv.ParseUint(client.room, 10, 32)
+	if err == nil {
+		// Fetch and send event
+		event, err := client.hub.eventService.FindOne(uint(eventId))
+		if err == nil {
+			eventJSON, err := json.Marshal(event)
+			if err == nil {
+				eventMessage, err := json.Marshal(Message{
+					Type:    "event",
+					Payload: eventJSON,
+				})
+				if err == nil {
+					client.hub.rooms[client.room].broadcast <- eventMessage
+				} else {
+					fmt.Println("Failed to marshal event message:", err)
+				}
+			} else {
+				fmt.Println("Failed to marshal event:", err)
+			}
+		} else {
+			fmt.Println("Failed to get event:", err)
+		}
+
+		// Fetch and send users
+		users, err := client.hub.eventService.GetUsers(uint(eventId))
+		if err == nil {
+			usersJSON, err := json.Marshal(users)
+			if err == nil {
+				usersMessage, err := json.Marshal(Message{
+					Type:    "users",
+					Payload: usersJSON,
+				})
+				if err == nil {
+					client.hub.rooms[client.room].broadcast <- usersMessage
+				} else {
+					fmt.Println("Failed to marshal users message:", err)
+				}
+			} else {
+				fmt.Println("Failed to marshal users:", err)
+			}
+		} else {
+			fmt.Println("Failed to get event users:", err)
+		}
+
+		// Fetch and send expenses
+		expenses, err := client.hub.expenseService.FindByEventId(uint(eventId))
+		if err == nil {
+			expensesJSON, err := json.Marshal(expenses)
+			if err == nil {
+				expensesMessage, err := json.Marshal(Message{
+					Type:    "expenses",
+					Payload: expensesJSON,
+				})
+				if err == nil {
+					client.hub.rooms[client.room].broadcast <- expensesMessage
+				} else {
+					fmt.Println("Failed to marshal expenses message:", err)
+				}
+			} else {
+				fmt.Println("Failed to marshal expenses:", err)
+			}
+		} else {
+			fmt.Println("Failed to get event expenses:", err)
+		}
+
+		balances, err := client.hub.eventService.GetBalances(event)
+		if err == nil {
+			balancesJSON, err := json.Marshal(balances)
+			if err == nil {
+				balancesMessage, err := json.Marshal(Message{
+					Type:    "balances",
+					Payload: balancesJSON,
+				})
+				if err == nil {
+					client.hub.rooms[client.room].broadcast <- balancesMessage
+				} else {
+					fmt.Println("Failed to marshal balances message:", err)
+				}
+			} else {
+				fmt.Println("Failed to marshal balances:", err)
+			}
+		} else {
+			fmt.Println("Failed to get event balances:", err)
+		}
+	}
 }
