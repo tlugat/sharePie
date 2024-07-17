ee0b9639fce6f51520453ee70ecd07bc0572ea83
tlugat <teo.lugat@ausha.co>
2024-07-17 21:53:01 +0200

    🐛 Fix create event input

--- internal/types/event.go
+++ internal/types/event.go
@@ -44,7 +44,7 @@ type IEventService interface {
 
 type CreateEventInput struct {
 	Name        string  `json:"name" binding:"required"`
-	Description string  `json:"description" binding:"required"`
+	Description string  `json:"description"`
 	Category    uint    `json:"category"`
 	Image       string  `json:"image"`
 	Goal        float64 `json:"goal"`
